5596f1313af4d061b3e6c28df64ac0fb4ec4d7ef
Daan Last <daanlast123@gmail.com>
2018-12-20 10:57:20 +0100

    Initial commit

---
+++ Editor/CopyValueTool.cs
@@ -0,0 +1,47 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Reflection.Simple;
+
+//An editor window for ValueCopyTool Makes all fields selectable from a pop-up list and adds a button to invoke the copy.
+public class CopyValueTool : EditorWindow
+{
+    private int copyFieldIndex;
+    private int pasteFieldIndex;
+    private MonoBehaviour copyTargetComponent;
+    private MonoBehaviour pasteTargetComponent;
+
+    [MenuItem("Window/Reflection/Copy Value Tool")]
+    static void Init()
+    {
+        CopyValueTool window = (CopyValueTool)GetWindow(typeof(CopyValueTool));
+        window.titleContent = new GUIContent("Copy Value Tool");
+        window.minSize = new Vector2(300, 100);
+        window.maxSize = new Vector2(300, 100);
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        copyTargetComponent = (MonoBehaviour)EditorGUILayout.ObjectField("Copy Script", copyTargetComponent, typeof(MonoBehaviour), true);
+        if (copyTargetComponent != null)
+        {
+            copyFieldIndex = EditorGUILayout.Popup("Copy Field", copyFieldIndex, Reflection.GetAllFieldNames(copyTargetComponent));
+        }
+
+        pasteTargetComponent = (MonoBehaviour)EditorGUILayout.ObjectField("Paste Script", pasteTargetComponent, typeof(MonoBehaviour), true);
+        if (pasteTargetComponent != null)
+        {
+            pasteFieldIndex = EditorGUILayout.Popup("Paste Field", pasteFieldIndex, Reflection.GetAllFieldNames(pasteTargetComponent));
+        }
+
+        if (GUILayout.Button("Copy values"))
+        {
+            CopyValues();
+        }
+    }
+
+    public void CopyValues()
+    {
+        Reflection.SetValue(pasteTargetComponent, pasteFieldIndex, Reflection.GetValue(copyTargetComponent, copyFieldIndex));
+    }
+}
---
+++ Scripts/Reflection.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+namespace System.Reflection.Simple
+{
+    //Static helper class that simplifies reflection by exposing field names.
+    public static class Reflection
+    {
+        public static object GetValue(object script, string targetField)
+        {
+            if (HasField(script, targetField))
+            {
+                return script.GetType().GetField(targetField).GetValue(script);
+            }
+            return null;
+        }
+
+        public static object GetValue(object script, int targetFieldIndex)
+        {
+            return script.GetType().GetFields()[targetFieldIndex].GetValue(script);
+        }
+
+        public static bool HasField(object script, string targetField)
+        {
+            FieldInfo field = script.GetType().GetField(targetField);
+            if (field != null)
+            {
+                return true;
+            }
+
+            Debug.Log("Couldn't find field " + targetField + " on " + script);
+            return false;
+        }
+
+        public static string[] GetAllFieldNames(object script)
+        {
+            FieldInfo[] fields = script.GetType().GetFields();
+            string[] variableNames = new string[fields.Length];
+
+            for (int i = 0; i < fields.Length; i++)
+            {
+                variableNames[i] = fields[i].Name;
+            }
+
+            return variableNames;
+        }
+
+        public static void SetValue(object script, string targetField, object newValue)
+        {
+            if (HasField(script, targetField))
+            {
+                script.GetType().GetField(targetField).SetValue(script, newValue);
+            }
+        }
+
+        public static void SetValue(object[] scripts, string targetField, object newValue)
+        {
+            for (int i = 0; i < scripts.Length; i++)
+            {
+                if (HasField(scripts[i], targetField))
+                {
+                    scripts[i].GetType().GetField(targetField).SetValue(scripts[i], newValue);
+                }
+            }
+        }
+
+        public static void SetValue(object script, string[] targetFields, object newValue)
+        {
+            for (int i = 0; i < targetFields.Length; i++)
+            {
+                if (HasField(script, targetFields[i]))
+                {
+                    script.GetType().GetField(targetFields[i]).SetValue(script, newValue);
+                }
+            }
+        }
+
+        public static void SetValue(object[] scripts, string[] targetFields, object newValue)
+        {
+            for (int i = 0; i < scripts.Length; i++)
+            {
+                for (int f = 0; f < targetFields.Length; f++)
+                {
+                    if (HasField(scripts[i], targetFields[f]))
+                    {
+                        scripts[i].GetType().GetField(targetFields[f]).SetValue(scripts[i], newValue);
+                    }
+                }
+            }
+        }
+
+        public static void SetValue(object script, int targetFieldIndex, object newValue)
+        {
+            script.GetType().GetFields()[targetFieldIndex].SetValue(script, newValue);
+        }
+
+        public static void SetValue(object[] scripts, int targetFieldIndex, object newValue)
+        {
+            for (int i = 0; i < scripts.Length; i++)
+            {
+                scripts[i].GetType().GetFields()[targetFieldIndex].SetValue(scripts[i], newValue);
+            }
+        }
+    }
+}
